0894a8cd8014f4af7bde7919fdcb2d4cd425aab9
Gülden Güngör <gldngngr.0@gmail.com>
2026-05-08 22:46:46 +0300

    Add setter methods to Makine class

--- src/model/Makine.java
+++ src/model/Makine.java
@@ -41,13 +41,39 @@ public class Makine {
         return kapasite;
     }
 
-
-
     public String getBakimPeriyodu() {
         return bakimPeriyodu;
     }
 
     public String getLokasyon() {
         return lokasyon;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMakineTipi(String makineTipi) {
+        this.makineTipi = makineTipi;
+    }
+
+    public void setMakineKodu(String makineKodu) {
+        this.makineKodu = makineKodu;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public void setKapasite(String kapasite) {
+        this.kapasite = kapasite;
+    }
+
+    public void setBakimPeriyodu(String bakimPeriyodu) {
+        this.bakimPeriyodu = bakimPeriyodu;
+    }
+
+    public void setLokasyon(String lokasyon) {
+        this.lokasyon = lokasyon;
     }
 }
